6237cdd0791a9dc87e7b404c76f2b39f0ebf46ce
Maximiliano Ossa <maxiossaaedo@gmail.com>
2026-04-22 18:13:42 -0400

    actualice el archivo .h con mas funicones que creo que necesitaremos

--- calculadora.h
+++ calculadora.h
@@ -2,13 +2,19 @@
 #include <string.h>
 
 struct panel {
-    char nombre[50];
+    char nombre[100];
+    char tipo_panel[50];
+    char tecnologia[50];
     int potencia;
-    int consumo;
+    int eficiencia;
     int precio;
-
 };
 
 // funciones necesarias por implementar
 
-void mostrarPanel(struct panel tipos[], int n);
+void mostrarPaneles(struct panel paneles[], int n);
+int seleccionarPanel();
+int calcularNpaneles(int consumo, int potenciaPanel);
+int calcularCosto(struct panel paneles[], int n_panel, int cantidad);
+void mostrarREsumen(struct panel paneles[], int n_panel, int cantidad, int costo);
+
